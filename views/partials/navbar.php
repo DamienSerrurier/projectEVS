83078ad70c22a7de1ed575dbd7fe672ef4041d69
Damien Serrurier <damien.ser@laposte.net>
2023-08-04 23:54:30 +0200

    Ajout du fichier navbar et du footer

---
+++ views/partials/navbar.php
@@ -0,0 +1,51 @@
+<header>
+  <div class="d-none d-xl-block">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#"><img src="../../assets/img/iconCoffee.jpg" alt="" width="40rem" height="50rem"></a>
+        <div class="row w-100">
+          <ul class="navbar-nav justify-content-between">
+            <div>
+              <li class="">
+                <a class="nav-link active" aria-current="page" href="home">Accueil</a>
+              </li>
+            </div>
+            <div class="row">
+              <li class="nav-item col-xl-7">
+                <a class="nav-link" href="accountCreation">Création compte</a>
+              </li>
+              <li class="nav-item col-xl-5">
+                <a class="nav-link" href="accountConnection">Connexion</a>
+              </li>
+            </div>
+          </ul>
+        </div>
+      </div>
+  </div>
+  </nav>
+  </div>
+
+  <div class="d-xl-none d-sm-block">
+    <nav class="navbar navbar-expand-lg bg-body-tertiary">
+      <div class="container-fluid">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+          <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+          <ul class="navbar-nav">
+            <li class="nav-item">
+              <a class="nav-link active" aria-current="page" href="home">Accueil</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="accountCreation">Création compte</a>
+            </li>
+            <li class="nav-item">
+              <a class="nav-link" href="accountConnection">Connexion</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</header>

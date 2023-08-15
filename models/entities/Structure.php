07b215620c4ad81525f14d18e9a27108d917aca2
DamienSerrurier <damien.ser@laposte.net>
2023-08-15 08:39:50 +0200

    Ajout des tests unitaires de la classe Structure ainsi que la modifications des setters conformément aux tests

--- models/entities/Structure.php
+++ models/entities/Structure.php
@@ -2,14 +2,16 @@
 
 namespace ProjectEvs;
 
-class Structure {
+require_once '../../utility/exceptions/ExceptionPerso.php';
+
+class Structure implements RegexTester {
 
     //Propriétés
     private int $id;
     private string $name;
     private string $logo;
     private string $phone;
-    private TypeStructure $typeSructure;
+    private TypeStructure $typeStructure;
     private Address $address;
 
     //Constructeur
@@ -20,7 +22,18 @@ class Structure {
     }
 
     public function setId(int $id) {
-        $this->id = $id;
+
+        if($id > 0) {
+
+            if (filter_var($id, FILTER_VALIDATE_INT)) {
+                return $this->id = $id;
+            } else {
+                throw new ExceptionPerso("Arrêtez de jouer avec mes post");
+            }
+        }
+        else {
+            throw new ExceptionPerso('La valeur doit être positif et supérieur à 0');
+        }
     }
 
     public function getName() : string {
@@ -28,7 +41,18 @@ class Structure {
     }
 
     public function setName(string $name) {
-        $this->name = $name;
+
+        if (!empty($name)) {
+            $pattern = '/^[a-zA-Z- éèêôâàîïùûç]+$/';
+
+            if ($this->testInput($pattern, $name)) {
+                return $this->name = $name;
+            } else {
+                throw new ExceptionPerso("Le nom de la structure n'est pas valide");
+            }
+        } else {
+            throw new ExceptionPerso("Veuillez renseigner ce champ");
+        }
     }
 
     public function getLogo() : string {
@@ -36,7 +60,41 @@ class Structure {
     }
 
     public function setLogo(string $logo) {
-        $this->logo = $logo;
+        $error = $_FILES['userfile']['error'];
+        
+        if ($error == 0) {
+            $path = '../../assets/test/';
+            $expectedType = ['image/png', 'image/jpeg'];
+            $mineType = mime_content_type($path . $logo);
+    
+            if (in_array($mineType, $expectedType)) {
+                $maxWidth = 1000;
+                $maxHeight = 1000;
+                list($width, $height) = getimagesize($path . $logo);
+    
+                if ($maxWidth >= $width || $maxHeight >= $height) {
+                    $maxSize = 10000;
+                    $fileSize = filesize($path . $logo);
+    
+                    if ($maxSize >= $fileSize) {
+                        $this->logo = $logo;
+                    }
+                    else {
+                        throw new ExceptionPerso("Votre fichier est trop lourd, la taille maximale est de 10ko");
+                    }
+                }
+                else {
+                    throw new ExceptionPerso("Veuillez réduire la taille de l'image à 1000x1000");
+                }
+            }
+            else {
+                throw new ExceptionPerso("Veuillez choisir un fichier image (png, jpeg / jpg ou GIF)");
+            }
+            
+        }
+        else {
+            throw new ExceptionPerso("Votre fichier n'a pu être envoyé, veuillez réessayer");
+        }
     }
 
     public function getPhone() : string {
@@ -44,15 +102,31 @@ class Structure {
     }
 
     public function setPhone(string $phone) {
-        $this->phone = $phone;
+        if (!empty($phone)) {
+            $pattern = '/^[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}$/';
+
+            if ($this->testInput($pattern, $phone)) {
+                return $this->phone = $phone;
+            } else {
+                throw new ExceptionPerso("Le numéro de téléphone n'est pas valide");
+            }
+        } else {
+            throw new ExceptionPerso("Veuillez renseigner ce champ");
+        }
     }
 
     public function getTypeStructure() : TypeStructure {
-        return $this->typeSructure;
+        return $this->typeStructure;
     }
 
     public function setTypeStructure(TypeStructure $typeStructure) {
-        $this->typeSructure = $typeStructure;
+
+        if ($typeStructure instanceof TypeStructure) {
+            return $this->typeStructure = $typeStructure;
+        }
+        else {
+            throw new ExceptionPerso("Ceci n'est pas une instance de la classe Avatar");
+        }
     }
 
     public function getAddress() : Address {
@@ -60,6 +134,16 @@ class Structure {
     }
 
     public function setAddress(Address $address) {
-        $this->address = $address;
+
+        if ($address instanceof Address) {
+            return $this->address = $address;
+        }
+        else {
+            throw new ExceptionPerso("Ceci n'est pas une instance de la classe Avatar");
+        }
+    }
+
+    public function testInput($pattern, $input) {
+        return preg_match($pattern, $input);
     }
 }
---
+++ tests/StructureTest.php
@@ -0,0 +1,212 @@
+<?php
+
+require_once '../../models/entities/Structure.php';
+require_once '../../models/entities/TypeStructure.php';
+
+use ProjectEvs\ExceptionPerso;
+use ProjectEvs\Structure;
+use ProjectEvs\TypeStructure;
+use ProjectEvs\Address;
+
+class StructureTest extends PHPUnit\Framework\TestCase {
+
+    //Test unitaire sur setId
+    public function testSetIdWithFilter() {
+        $structure = new Structure();
+        $id = 1;
+        $badNumber = 0;
+        $this->assertGreaterThan($badNumber, $id);
+        $structure->setId($id);
+        $this->assertSame(filter_var($structure->getid(), FILTER_VALIDATE_INT), $structure->getid());
+    }
+
+    public function testSetIdNotMatchingWithFilter() {
+        $this->expectException(ExceptionPerso::class);
+        $structure = new Structure();
+        $id = -356;
+        $structure->setId($id);
+    }
+
+    //Test unitaire sur setName
+    public function testSetNameWithRegex() {
+        $structure = new Structure();
+        $name = 'djsjd-ejjdéèUY';
+        $pattern = '/^[a-zA-Z- éèêôâàîïùûç]+$/';
+        $this->assertMatchesRegularExpression($pattern, $structure->setName($name));
+    }
+
+    public function testSetNameWithNumber() {
+        $this->expectException(ExceptionPerso::class);
+        $structure = new Structure();
+        $name = 426;
+        $structure->setName($name);
+    }
+
+    public function testSetNameWithSpecialCharacter() {
+        $this->expectException(ExceptionPerso::class);
+        $structure = new Structure();
+        $name = '?"/!';
+        $structure->setName($name);
+    }
+
+    public function testNotMatchNameRegex() {
+        $this->expectException(ExceptionPerso::class);
+        $structure = new Structure();
+        $name = 'hdjjj-Gl56Msjjs';
+        $structure->setName($name);
+    }
+
+    public function testSetNameIsNotEmpty() {
+        $structure = new Structure();
+        $name = 'sjsèéêdY';
+        $this->assertNotEmpty($structure->setName($name, ''));
+    }
+
+    //Test unitaire sur setLogo
+    private static function simulateFileUpload (
+        $name,
+        $tmpName,
+        $error 
+    ) {
+        $_FILES['userfile'] = [
+            'name'     => $name,
+            'tmp_name' => $tmpName,
+            'error'    => $error,
+        ];
+    }
+
+    public function testSetLogoErrorAuther() {
+        $this->expectException(ExceptionPerso::class);
+        $this->simulateFileUpload('invalidFile.txt', '../../assets/test/', UPLOAD_ERR_FORM_SIZE);
+        $structure = new Structure();
+        $error = 0;
+        $fileError = $_FILES['userfile']['error'];
+        $file = $_FILES['userfile']['name'];
+        $this->assertNotEquals($error, $fileError);
+        $structure->setLogo($file);
+    }
+
+    public function testSetLogoErrorEqualZero() {
+        $this->simulateFileUpload('logoCaf.png', '../../assets/test/', UPLOAD_ERR_OK);
+        $structure = new Structure();
+        $error = 0;
+        $fileError = $_FILES['userfile']['error'];
+        $file = $_FILES['userfile']['name'];
+        $this->assertEquals($error, $fileError);
+        $structure->setLogo($file);
+    }
+
+    public function testSetLogoMimeType() {
+        $structure = new Structure();
+        $path = '../../assets/test/';
+        $file = 'logoCaf.png';
+        $expectedMimeContentType = ['image/png', 'image/jpeg'];
+        $mimeType = mime_content_type($path . $file);
+        $structure->setLogo($file);
+        $this->assertEquals(in_array($mimeType, $expectedMimeContentType), $mimeType);
+    }
+
+    public function testSetLogoBadMimeType() {
+        $this->expectException(ExceptionPerso::class);
+        $structure = new Structure();
+        $file = 'invalidFile.txt';
+        $structure->setLogo($file);
+    }
+
+    public function testSetLogoDimensions() {
+        $structure = new Structure();
+        $maxWidth = 1000;
+        $maxHeight = 1000;
+        $path = '../../assets/test/';
+        $file = 'logoCaf.png';
+        list($width, $height) = getimagesize($path . $file);
+        $this->assertLessThanOrEqual($maxWidth, $width);
+        $this->assertLessThanOrEqual($maxHeight, $height);
+        $structure->setLogo($file);
+    }
+
+    public function testSetLogoBadDimension() {
+        $this->expectException(ExceptionPerso::class);
+        $structure = new Structure();
+        $maxWidth = 1000;
+        $maxHeight = 1000;
+        $path = '../../assets/test/';
+        $file = 'iconCoffee.jpg';
+        list($width, $height) = getimagesize($path . $file);
+        $this->assertGreaterThan($maxWidth, $width);
+        $this->assertGreaterThan($maxHeight, $height);
+        $structure->setLogo($file);
+    }
+
+    public function testSetLogoSize() {
+        $structure = new Structure();
+        $maxSize = 10000;
+        $path = '../../assets/test/';
+        $file = 'logoCaf.png';
+        $fileSize = filesize($path . $file);
+        $this->assertLessThanOrEqual($maxSize, $fileSize);
+        $structure->setLogo($file);
+    }
+
+    public function testSetLogoBadSize() {
+        $this->expectException(ExceptionPerso::class);
+        $structure = new Structure();
+        $maxSize = 10000;
+        $path = '../../assets/test/';
+        $file = 'iconCoffee.jpg';
+        $fileSize = filesize($path . $file);
+        $this->assertGreaterThan($maxSize, $fileSize);
+        $structure->setLogo($file);
+    }
+
+    //Test unitaire sur setPhone
+    public function testSetPhoneWithRegex() {
+        $structure = new Structure();
+        $phone = '05 56 23 07 51';
+        $pattern = '/^[0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2} [0-9]{2}$/';
+        $this->assertMatchesRegularExpression($pattern, $structure->setPhone($phone));
+    }
+
+    public function testSetPhoneWithNumber() {
+        $this->expectException(ExceptionPerso::class);
+        $structure = new Structure();
+        $phone = 426;
+        $structure->setPhone($phone);
+    }
+
+    public function testSetPhoneWithSpecialCharacter() {
+        $this->expectException(ExceptionPerso::class);
+        $structure = new Structure();
+        $phone = '?"/!';
+        $structure->setPhone($phone);
+    }
+
+    public function testNotMatchPhoneRegex() {
+        $this->expectException(ExceptionPerso::class);
+        $structure = new Structure();
+        $phone = 'hdjjj-Gl56Msjjs';
+        $structure->setPhone($phone);
+    }
+
+    public function testSetPhoneIsNotEmpty() {
+        $structure = new Structure();
+        $name = '05 56 23 07 51';
+        $this->assertNotEmpty($structure->setPhone($name, ''));
+    }
+
+    //Test unitaire sur setTypeStructure
+    public function testSetTypeStructureInstanceOf() {
+        $structure = new Structure();
+        $typeStructure = new TypeStructure();
+        $structure->setTypeStructure($typeStructure);
+        $this->assertInstanceOf(TypeStructure::class, $structure->getTypeStructure());
+    }
+
+    //Test unitaire sur setAddress
+    public function testSetAddressInstanceOf() {
+        $structure = new Structure();
+        $address = new Address();
+        $structure->setAddress($address);
+        $this->assertInstanceOf(Address::class, $structure->getAddress());
+    }
+}

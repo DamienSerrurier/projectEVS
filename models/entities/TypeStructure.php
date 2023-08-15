b5a7dde6e516b29395396e49041b92e14dd59215
DamienSerrurier <damien.ser@laposte.net>
2023-08-15 09:22:30 +0200

    Ajout des tests unitaires de la classe TypeStructure ainsi que la modifications des setters conformément aux tests

--- models/entities/TypeStructure.php
+++ models/entities/TypeStructure.php
@@ -2,7 +2,9 @@
 
 namespace ProjectEvs;
 
-class TypeStructure {
+require_once '../../utility/exceptions/ExceptionPerso.php';
+
+class TypeStructure implements RegexTester {
 
     //Propriétés
     private int $id;
@@ -16,7 +18,18 @@ class TypeStructure {
     }
 
     public function setId(int $id) {
-        $this->id = $id;
+
+        if($id > 0) {
+
+            if (filter_var($id, FILTER_VALIDATE_INT)) {
+                return $this->id = $id;
+            } else {
+                throw new ExceptionPerso("Arrêtez de jouer avec mes post");
+            }
+        }
+        else {
+            throw new ExceptionPerso('La valeur doit être positif et supérieur à 0');
+        }
     }
 
     public function getWording() : string {
@@ -24,6 +37,21 @@ class TypeStructure {
     }
 
     public function setWording(string $wording) {
-        $this->wording = $wording;
+
+        if (!empty($wording)) {
+            $pattern = '/^[a-zA-Z- éèêôâàîïùûç]+$/';
+
+            if ($this->testInput($pattern, $wording)) {
+                return $this->wording = $wording;
+            } else {
+                throw new ExceptionPerso("Le nom de la type de structure n'est pas valide");
+            }
+        } else {
+            throw new ExceptionPerso("Veuillez renseigner ce champ");
+        }
+    }
+
+    public function testInput($pattern, $input) {
+        return preg_match($pattern, $input);
     }
 }
---
+++ tests/TypeStructureTest.php
@@ -0,0 +1,63 @@
+<?php
+
+require_once '../../models/entities/TypeStructure.php';
+
+use ProjectEvs\ExceptionPerso;
+use ProjectEvs\TypeStructure;
+
+class TypeStructureTest extends PHPUnit\Framework\TestCase {
+
+    //Test unitaire sur setId
+    public function testSetIdWithFilter() {
+        $typeStructure = new TypeStructure();
+        $id = 1;
+        $badNumber = 0;
+        $this->assertGreaterThan($badNumber, $id);
+        $typeStructure->setId($id);
+        $this->assertSame(filter_var($typeStructure->getid(), FILTER_VALIDATE_INT), $typeStructure->getid());
+    }
+
+    public function testSetIdNotMatchingWithFilter() {
+        $this->expectException(ExceptionPerso::class);
+        $typeStructure = new TypeStructure();
+        $minNuber = 1;
+        $id = -356;
+        $this->assertLessThan($minNuber, $id);
+        $typeStructure->setId($id);
+    }
+
+    //Test unitaire sur setWording
+    public function testSetWordingWithRegex() {
+        $typeStructure = new TypeStructure();
+        $Wording = 'djsjd-ejjdéèUY';
+        $pattern = '/^[a-zA-Z- éèêôâàîïùûç]+$/';
+        $this->assertMatchesRegularExpression($pattern, $typeStructure->setWording($Wording));
+    }
+
+    public function testSetWordingWithNumber() {
+        $this->expectException(ExceptionPerso::class);
+        $typeStructure = new TypeStructure();
+        $Wording = 426;
+        $typeStructure->setWording($Wording);
+    }
+
+    public function testSetWordingWithSpecialCharacter() {
+        $this->expectException(ExceptionPerso::class);
+        $typeStructure = new TypeStructure();
+        $Wording = '?"/!';
+        $typeStructure->setWording($Wording);
+    }
+
+    public function testNotMatchWordingRegex() {
+        $this->expectException(ExceptionPerso::class);
+        $typeStructure = new TypeStructure();
+        $Wording = 'hdjjj-Gl56Msjjs';
+        $typeStructure->setWording($Wording);
+    }
+
+    public function testSetWordingIsNotEmpty() {
+        $typeStructure = new TypeStructure();
+        $Wording = 'sjsèéêdY';
+        $this->assertNotEmpty($typeStructure->setWording($Wording, ''));
+    }
+}

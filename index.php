58df069e2bf44a20b4416e2dd4a8f427c6982abf
DamienSerrurier <damien.ser@laposte.net>
2023-08-17 13:53:33 +0200

    Ajouts des liens du routeur

--- index.php
+++ index.php
@@ -9,3 +9,15 @@ else if (isset($_GET['page']) && $_GET['page'] == 'accountCreation') {
 else if (isset($_GET['page']) && $_GET['page'] == 'accountConnection') {
     require getcwd() . '/controllers/accountConnectionController.php';
 }
+else if (isset($_GET['page']) && $_GET['page'] == 'accountLogout') {
+    require getcwd() . '/controllers/accountLogoutController.php';
+}
+else if (isset($_GET['page']) && $_GET['page'] == 'userSpace') {
+    require getcwd() . '/controllers/userSpaceController.php';
+}
+else if (isset($_GET['page']) && $_GET['page'] == 'educationalEstablishment') {
+    require getcwd() . '/controllers/educationalEstablishmentController.php';
+}
+else if (isset($_GET['page']) && $_GET['page'] == 'administratorSpace') {
+    require getcwd() . '/controllers/administratorSpaceController.php';
+}

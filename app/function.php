c5f4f524e63168284ea147f05ee5fba856cf2528
DamienSerrurier <damien.ser@laposte.net>
2023-08-18 18:38:57 +0200

    Modification function pour rajouter autant de token qu'il y a de formulaire dans une page

--- app/function.php
+++ app/function.php
@@ -1,13 +1,13 @@
 <?php
 
-function sessionStartWithGenerateToken() {
+function sessionStartWithGenerateToken($tokenName) {
 
     if (!session_id()) {
         session_start();
         session_regenerate_id(true);
     }
 
-    if (empty($_SESSION['token'])) {
-        $_SESSION['token'] = bin2hex(random_bytes(32));
+    if (empty($_SESSION[$tokenName])) {
+        $_SESSION[$tokenName] = bin2hex(random_bytes(32));
     }
 }
